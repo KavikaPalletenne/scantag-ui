42b876b3b21c6b372335d841fa11f13f156812a3
Kavika <kbpalletenne@gmail.com>
2021-04-17 17:04:44 +1000

    minor changes

--- pages/index.js
+++ pages/index.js
@@ -27,10 +27,10 @@ export default function Home() {
                 </Link>
                 <div class="pt-2">
                     <Link href="/register">
-                        <a class="md:text-xl font-semibold float-right md:rounded-3xl md:pl-9 md:pr-9 md:pt-3 md:pb-3 text-lg pl-5 pr-5 pt-2 pb-2 rounded-3xl" style={{backgroundColor: "#FF9369"}}>Sign Up</a>
+                        <a class="md:text-xl font-semibold float-right md:rounded-3xl md:pl-6 md:pr-6 md:pt-2 md:pb-2 text-lg pl-5 pr-5 pt-2 pb-2 rounded-3xl" style={{backgroundColor: "#FF9369"}}>Sign Up</a>
                     </Link>
                     <Link href="/login">
-                        <a class="md:text-xl font-semibold float-right md:pt-3 md:pr-5 text-lg pt-2 pr-3">Login</a>
+                        <a class="md:text-xl font-semibold float-right md:pt-2 md:pr-5 text-lg pt-2 pr-3">Login</a>
                     </Link>
                 </div>
             </div>
@@ -40,9 +40,9 @@ export default function Home() {
                     <div class="w-full md:w-1/2">
                         <div class="max-w-2xl pr-4">
                             <h1 class="text-3xl font-bold text-white dark:text-white md:text-7xl" style={{fontFamily: 'Rubik'}}>The modern name tag you've been missing.</h1>
-                            <p class="mt-2 text-gray-300 dark:text-gray-400 pb-10">The simple way for anyone to assist locating their lost items.</p>
+                            <p class="mt-2 md:text-2xl text-gray-300 dark:text-gray-400 pb-10">The simple way for anyone to assist locating their lost items.</p>
                             <Link href="/register">
-                                <a class="md:text-xl text-white font-semibold md:rounded-3xl md:pl-9 md:pr-9 md:pt-3 md:pb-3 text-lg pl-5 pr-5 pt-2 pb-2 rounded-3xl" style={{backgroundColor: "#FF9369"}}>Sign Up</a>
+                                <a class="md:text-3xl text-white font-semibold md:rounded-full md:pl-9 md:pr-9 md:pt-3 md:pb-3 text-lg pl-5 pr-5 pt-2 pb-2 rounded-3xl" style={{backgroundColor: "#FF9369"}}>Sign Up</a>
                             </Link>
                         </div>
                     </div>
