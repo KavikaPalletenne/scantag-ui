b00eb267ebed20b1e3691097a3a4c480ea561cd3
Kavika <kbpalletenne@gmail.com>
2021-04-17 16:58:06 +1000

    home page finished

--- pages/index.js
+++ pages/index.js
@@ -53,8 +53,95 @@ export default function Home() {
                 </div>
             </div>
         </header>
-
         
+        <section>
+            <div class="container max-w-7xl mx-auto pb-20 lg:pb-40 p-4 sm:p-6 pt-20 lg:pt-20 lg:p-8 bg-white dark:bg-gray-800">
+                <div class="flex flex-wrap -mx-4">
+                    <div class="w-full lg:w-1/2 px-8">
+                        <div class="mb-12 lg:pt-10 lg:mb-0 pb-12 lg:pb-0 border-b lg:border-b-0">
+                            <h2 class="mb-4 text-3xl lg:text-4xl font-bold font-heading dark:text-white">
+                                Ease of use, peace of mind.
+                            </h2>
+                            <p class="mb-8 leading-loose text-gray-500 dark:text-gray-300">
+                                Simply register your account, then input your desired information. After this your personalised QR code will be generated. Use this on anything you wish; your wallet, luggage, even your dog. Whenever someone has scans your code, we send you a notification so you know if someones found it. We even added messaging into the site so people can contact you faster than ever before.
+                            </p>
+                        </div>
+                    </div>
+                    <div class="w-full lg:w-1/2 px-8">
+                        <ul class="space-y-12">
+                            <li class="flex -mx-4">
+                                <div class="px-4">
+                                    <span class="flex w-16 h-16 mx-auto items-center justify-center text-2xl font-bold font-heading rounded-full" style={{backgroundColor: "#ffebe3", color: "#FF9369"}}>
+                                        1
+                                    </span>
+                                </div>
+                                <div class="px-4">
+                                    <h3 class="my-1 text-xl font-semibold dark:text-white">
+                                        Easily customise your information at anytime, on any device.
+                                    </h3>
+                                </div>
+                            </li>
+                            <li class="flex -mx-4">
+                                <div class="px-4">
+                                    <span class="flex w-16 h-16 mx-auto items-center justify-center text-2xl font-bold font-heading rounded-full" style={{backgroundColor: "#ffebe3", color: "#FF9369"}}>
+                                        2
+                                    </span>
+                                </div>
+                                <div class="px-4">
+                                    <h3 class="my-1 text-xl font-semibold dark:text-white">
+                                        With no upfront costs, you only pay when you need.
+                                    </h3>
+                                </div>
+                            </li>
+                            <li class="flex -mx-4">
+                                <div class="px-4">
+                                    <span class="flex w-16 h-16 mx-auto items-center justify-center text-2xl font-bold font-heading rounded-full" style={{backgroundColor: "#ffebe3", color: "#FF9369"}}>
+                                        3
+                                    </span>
+                                </div>
+                                <div class="px-4">
+                                    <h3 class="my-1 text-xl font-semibold dark:text-white">
+                                        We keep your private and personal information locked and secure. 
+                                    </h3>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </section>
+        
+        <footer class="dark:bg-gray-800 w-full py-8" style={{backgroundColor: "#FF9369"}}>
+            <div class="max-w-screen-xl mx-auto px-4">
+                <ul class="max-w-screen-md mx-auto text-lg font-light flex flex-wrap justify-between">
+                    <li class="my-2">
+                        <a class="text-white hover:text-gray-200 dark:text-gray-300 dark:hover:text-white transition-colors duration-200" href="/faq">
+                            FAQ
+                        </a>
+                    </li>
+                    <li class="my-2">
+                        <a class="text-white hover:text-gray-200 dark:text-gray-300 dark:hover:text-white transition-colors duration-200" href="/site-map">
+                            Site Map
+                        </a>
+                    </li>
+                    <li class="my-2">
+                        <a class="text-white hover:text-gray-200 dark:text-gray-300 dark:hover:text-white transition-colors duration-200" href="/contact">
+                            Contact
+                        </a>
+                    </li>
+                    <li class="my-2">
+                        <a class="text-white hover:text-gray-200 dark:text-gray-300 dark:hover:text-white transition-colors duration-200" href="/legal">
+                            Legal
+                        </a>
+                    </li>
+                </ul>
+                
+                <div class="text-center text-white dark:text-gray-200 pt-10 sm:pt-12 font-light flex items-center justify-center">
+                    Created by <a href="https://kavika.dev" className="pl-1 hover:text-gray-200">Kavika</a>
+                </div>
+            </div>
+        </footer>
+
       </div>
   )
 }
