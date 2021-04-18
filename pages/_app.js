1f94bdfdc7edff8b1fb21b8fae958714723e139e
Kavika <kbpalletenne@gmail.com>
2021-04-18 20:04:13 +1000

    added loading bar

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,9 @@
 import '../styles/index.css'
 
+import Router from 'next/router';
+import NProgress from 'nprogress'; //nprogress module
+import '../styles/custom.nprogress.css'; //styles of nprogress//Binding events. 
+Router.events.on('routeChangeStart', () => NProgress.start()); Router.events.on('routeChangeComplete', () => NProgress.done()); Router.events.on('routeChangeError', () => NProgress.done());
 
 function MyApp({ Component, pageProps }) {
   return ( 
