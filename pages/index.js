6068b5aa6ed55e4ee16bf6565e6ccea6a3dee33b
Kavika <kbpalletenne@gmail.com>
2021-04-18 16:31:48 +1000

    improved SEO

--- pages/index.js
+++ pages/index.js
@@ -9,10 +9,10 @@ export default function Home() {
 
         <Head>
           <meta charset="utf-8" />
-          <meta name="description" content="ScanTag allows you to easily locate your lost items. Whenever someone scans your QR code, we send you a notification. The finder can also see your contact details on your personal ScanTag page." />
+          <meta name="description" content="ScanTag allows you to easily find your lost items. Whenever someone scans your QR code, we send you a notification. The finder can also see your contact details on your personal ScanTag page." />
           <meta name="robots" content="all" />
           <meta name="googlebot" content="all" />
-          <title>ScanTag - The Modern Name Tag</title>
+          <title>ScanTag - Find Your Lost Items</title>
           <link rel="icon" href="https://raw.githubusercontent.com/KavikaPalletenne/scantag-assets/main/scantag-logo.svg" />
         </Head>
 
