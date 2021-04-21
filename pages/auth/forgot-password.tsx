41568b9bc0b02baa949695fb3599163a5be5e15b
Kavika Palletenne <kbpalletenne@gmail.com>
2021-04-21 14:29:21 +1000

    added link to homepage

--- pages/auth/forgot-password.tsx
+++ pages/auth/forgot-password.tsx
@@ -38,7 +38,9 @@ export default function Home() {
         <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
             <div className="max-w-md w-full space-y-8">
                 <div>
-                <img className="mx-auto h-100 w-auto" src="https://raw.githubusercontent.com/KavikaPalletenne/scantag-assets/main/scantag-logo.svg" alt="Workflow"/>
+                <Link href="/">
+                    <img className="mx-auto h-100 w-auto" src="https://raw.githubusercontent.com/KavikaPalletenne/scantag-assets/main/scantag-logo.svg" alt="Workflow"/>
+                </Link>
                 <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900" style={{fontFamily: "Rubik"}}>
                     Forgot your password?
                 </h2>
