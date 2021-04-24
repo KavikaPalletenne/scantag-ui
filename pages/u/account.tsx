9e7ea8bddad4307d1ecfe2328baeab619d896313
Kavika Palletenne <kbpalletenne@gmail.com>
2021-04-24 16:53:09 +1000

    minimal account details display page

--- pages/u/account.tsx
+++ pages/u/account.tsx
@@ -1,8 +1,71 @@
-const jwt = localStorage.getItem('token')
+import Head from 'next/head'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+import {SyntheticEvent, useEffect, useState} from  'react'
 
 
-useEffect(() => {
-    if(jwt == null) {
-        router.push('/login')
-    }
-})
+export default function AuthFlowNew() {
+    
+    const router = useRouter()
+    
+    // User Variables
+    const [jwt, setJwt] = useState('')
+    const [userId, setUserId] = useState('')
+    const [email, setEmail] = useState('')
+    const [firstName, setFirstname] = useState('')
+    const [lastName, setLastName] = useState('')
+    const [contactNumber, setContactNumber] = useState('')
+    const [address, setAddress] = useState('')
+    const [info, setInfo] = useState('')
+    const [enableNotifications, setEnableNotifications] = useState(true)
+
+
+    useEffect(() => {
+        
+        setJwt(localStorage.getItem('token'))
+
+        async function getUser() {
+        
+            await fetch("https://api.scantag.co/api/v1/users/get/current", {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': 'Bearer ' + jwt
+                }
+            }).then(function(response) {
+                return response.json();
+            }).then(function(json) {  
+                
+                setUserId(json.userId)
+                localStorage.setItem('userId', userId)
+
+                setFirstname(json.firstName)
+                setLastName(json.lastName)
+                setEmail(json.email)
+                setContactNumber(json.contactNumber)
+                setAddress(json.address)
+                setInfo(json.info)
+                setEnableNotifications(json.enableNotifications)
+            });
+
+        }
+
+        getUser()
+    })
+
+    
+    return (
+      
+    <div>
+        <h1>{jwt}</h1>
+        <h1>{firstName}</h1>
+        <h1>{lastName}</h1>
+        <h1>{email}</h1>
+        <h1>{contactNumber}</h1>
+        <h1>{info}</h1>
+        <h1>{enableNotifications}</h1>
+
+
+    </div>
+  )
+}
