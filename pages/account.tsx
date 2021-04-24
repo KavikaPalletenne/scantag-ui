702bdee9c87054c5313b6885b0c1b7a684903943
Kavika Palletenne <kbpalletenne@gmail.com>
2021-04-24 21:06:27 +1000

    added details fetching

--- pages/account.tsx
+++ pages/account.tsx
@@ -12,14 +12,16 @@ export default function AuthFlowNew() {
     const [jwt, setJwt] = useState('')
     const [userId, setUserId] = useState('')
     const [email, setEmail] = useState('')
-    const [firstName, setFirstname] = useState('')
+    const [firstName, setFirstName] = useState('')
     const [lastName, setLastName] = useState('')
     const [contactNumber, setContactNumber] = useState('')
     const [address, setAddress] = useState('')
     const [info, setInfo] = useState('')
     const [enableNotifications, setEnableNotifications] = useState(true)
+    const [role, setRole] = useState('')
 
     var tempJwt
+    var tempUserId
 
     useEffect(() => {
         
@@ -29,13 +31,16 @@ export default function AuthFlowNew() {
 
         setJwt(tempJwt)
 
+        var bearer = 'Bearer ' + tempJwt;
+
         async function getUser() {
 
             await fetch("https://api.scantag.co/api/v1/users/get/current", {
                 method: 'GET',
+                credentials: 'include',
                 headers: {
                     'Content-Type': 'application/json',
-                    'Authorization': 'Bearer ' + tempJwt
+                    'Authorization': bearer
                 }
             }).then(function(response) {                
                 if(response.ok == false) {
@@ -55,13 +60,17 @@ export default function AuthFlowNew() {
                     setUserId(json.userId)
                     localStorage.setItem('userId', userId)
 
-                    setFirstname(json.firstName)
+                    setFirstName(json.firstName)
                     setLastName(json.lastName)
                     setEmail(json.email)
                     setContactNumber(json.contactNumber)
                     setAddress(json.address)
                     setInfo(json.info)
                     setEnableNotifications(json.enableNotifications)
+                    setRole(json.role)
+
+                    tempUserId = json.userId
+                    
                 }
             });
             
@@ -70,18 +79,107 @@ export default function AuthFlowNew() {
         getUser()
         return () => { isMounted=false }
     })
+    
+    const submit = async (e: SyntheticEvent) => {
+        e.preventDefault();
+        
+        var userUpdateId = "https://api.scantag.co/api/v1/users/update?id=" + userId
+
+        
+        await fetch(`https://api.scantag.co/api/v1/users/update?id=${userId}`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + tempJwt
+            },
+            body: JSON.stringify({
+                'email': email,
+                'firstName': firstName,
+                'lastName': lastName,
+                'contactNumber': contactNumber,
+                'address': address,
+                'role': role
+            })
+        })
+        
+        console.log("Should have fetched")
+        
+    }
+
 
     
     return (
       
     <div>
-        <h1>{jwt}</h1>
-        <h1>{firstName}</h1>
-        <h1>{lastName}</h1>
-        <h1>{email}</h1>
-        <h1>{contactNumber}</h1>
-        <h1>{info}</h1>
-        <h1>{enableNotifications}</h1>
+        
+        
+        <header>
+            
+            <div className="md:pt-10 md:pl-32 text-white md:pr-32 pt-10 pl-5 pr-5">
+                <Link  href="/account">
+                    <a>
+                        <img className="float-left md:w-20 w-12" src="https://raw.githubusercontent.com/KavikaPalletenne/scantag-assets/main/scantag-logo.svg"/>
+                    </a>
+                </Link>
+                <div className="pt-2">
+                    <Link href="/auth/logout">
+                        <a className="md:text-xl font-semibold float-right md:rounded-3xl md:pl-6 md:pr-6 md:pt-2 md:pb-2 text-lg pl-5 pr-5 pt-2 pb-2 rounded-3xl bg-orange hover:bg-orange-light transition-colors duration-200">Logout</a>
+                    </Link>
+                </div>
+            </div>
+
+            
+        </header>
+
+
+
+
+
+        <div className="mt-10 sm:mt-0">
+            <div className="max-w-3xl md:m-auto md:w-1/2 md:py-32">
+                
+                <div className="mt-5 md:mt-0 md:col-span-2">
+                <form onSubmit={submit}>
+                    <div className="shadow overflow-hidden sm:rounded-md">
+                    <div className="px-4 py-5 bg-white sm:p-6">
+                        <div className="grid grid-cols-6 gap-6">
+                        <div className="col-span-6 sm:col-span-3">
+                            <label htmlFor="firstname" className="block text-sm font-medium text-gray-700">First name</label>
+                            <input type="text" defaultValue={firstName} name="firstname" id="firstname" autoComplete="given-name" className="mt-1 focus:ring-orange focus:border-orange block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" onChange={e => setFirstName(e.target.value)}/>
+                        </div>
+
+                        <div className="col-span-6 sm:col-span-3">
+                            <label htmlFor="lastname" className="block text-sm font-medium text-gray-700">Last name</label>
+                            <input type="text" defaultValue={lastName} name="lastname" id="lastname" autoComplete="family-name" className="mt-1 focus:ring-orange focus:border-orange block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" onChange={e => setLastName(e.target.value)}/>
+                        </div>
+
+                        <div className="col-span-6 sm:col-span-4">
+                            <label htmlFor="emailaddress" className="block text-sm font-medium text-gray-700">Email address</label>
+                            <input type="text" defaultValue={email} name="emailaddress" id="emailaddress" autoComplete="email" className="mt-1 focus:ring-orange focus:border-orange block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" onChange={e => setEmail(e.target.value)}/>
+                        </div>
+
+                        <div className="col-span-6">
+                            <label htmlFor="streetaddress" className="block text-sm font-medium text-gray-700">Street address</label>
+                            <input type="text" defaultValue={address} name="streetaddress" id="streetaddress" autoComplete="street-address" className="mt-1 focus:ring-orange focus:border-orange block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" onChange={e => setAddress(e.target.value)}/>
+                        </div>
+                      
+                        <div className="col-span-6 sm:col-span-3 lg:col-span-2">
+                            <label htmlFor="mobilenumber" className="block text-sm font-medium text-gray-700">Mobile Number</label>
+                            <input type="text" defaultValue={contactNumber} name="mobilenumber" id="mobilenumber" autoComplete="tel" className="mt-1 focus:ring-orange focus:border-orange block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" onChange={e => setContactNumber(e.target.value)}/>
+                        </div>
+                        </div>
+                    </div>
+                    <div className="px-4 py-3 bg-gray-50 text-right sm:px-6">
+                        <button type="submit" className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-orange hover:bg-orange-light">
+                        Save
+                        </button>
+                    </div>
+                    </div>
+                </form>
+                </div>
+            </div>
+            </div>
+
 
 
     </div>
--- pages/index.js
+++ pages/index.js
@@ -19,88 +19,88 @@ export default function Home() {
         
         <header style={{backgroundImage: `url(https://raw.githubusercontent.com/KavikaPalletenne/scantag-assets/main/aurora-background4.png)`}}>
             
-            <div class="md:pt-10 md:pl-32 text-white md:pr-32 pt-10 pl-5 pr-5">
+            <div className="md:pt-10 md:pl-32 text-white md:pr-32 pt-10 pl-5 pr-5">
                 <Link  href="/">
                     <a>
-                        <img class="float-left md:w-20 w-12" src="https://raw.githubusercontent.com/KavikaPalletenne/scantag-assets/main/scantag-logo.svg"/>
+                        <img className="float-left md:w-20 w-12" src="https://raw.githubusercontent.com/KavikaPalletenne/scantag-assets/main/scantag-logo.svg"/>
                     </a>
                 </Link>
-                <div class="pt-2">
+                <div className="pt-2">
                     <Link href="/register">
-                        <a class="md:text-xl font-semibold float-right md:rounded-3xl md:pl-6 md:pr-6 md:pt-2 md:pb-2 text-lg pl-5 pr-5 pt-2 pb-2 rounded-3xl bg-orange hover:bg-orange-light transition-colors duration-200">Sign Up</a>
+                        <a className="md:text-xl font-semibold float-right md:rounded-3xl md:pl-6 md:pr-6 md:pt-2 md:pb-2 text-lg pl-5 pr-5 pt-2 pb-2 rounded-3xl bg-orange hover:bg-orange-light transition-colors duration-200">Sign Up</a>
                     </Link>
                     <Link href="/login">
-                        <a class="md:text-xl font-semibold float-right md:pt-2 md:pr-5 text-lg pt-2 pr-3">Login</a>
+                        <a className="md:text-xl font-semibold float-right md:pt-2 md:pr-5 text-lg pt-2 pr-3">Login</a>
                     </Link>
                 </div>
             </div>
 
-            <div class="container md:pl-32 md:pt-28 pt-24 mx-auto pl-5">
-                <div class="items-center md:flex">
-                    <div class="w-full md:w-1/2">
-                        <div class="max-w-2xl pr-4">
-                            <h1 class="text-3xl font-bold text-white dark:text-white md:text-7xl" style={{fontFamily: 'Rubik'}}>The modern name tag you've been missing.</h1>
-                            <p class="mt-2 md:text-4xl text-xl text-gray-300 dark:text-gray-400 pb-10">Find you lost items quicker.</p>
+            <div className="container md:pl-32 md:pt-28 pt-24 mx-auto pl-5">
+                <div className="items-center md:flex">
+                    <div className="w-full md:w-1/2">
+                        <div className="max-w-2xl pr-4">
+                            <h1 className="text-3xl font-bold text-white dark:text-white md:text-7xl" style={{fontFamily: 'Rubik'}}>The modern name tag you've been missing.</h1>
+                            <p className="mt-2 md:text-4xl text-xl text-gray-300 dark:text-gray-400 pb-10">Find you lost items quicker.</p>
                             <Link href="/register">
-                                <a class="md:text-3xl text-white font-semibold md:rounded-full md:pl-9 md:pr-9 md:pt-3 md:pb-3 text-lg pl-5 pr-5 pt-2 pb-2 rounded-3xl bg-orange hover:bg-orange-light transition-colors duration-200">Sign Up</a>
+                                <a className="md:text-3xl text-white font-semibold md:rounded-full md:pl-9 md:pr-9 md:pt-3 md:pb-3 text-lg pl-5 pr-5 pt-2 pb-2 rounded-3xl bg-orange hover:bg-orange-light transition-colors duration-200">Sign Up</a>
                             </Link>
                         </div>
                     </div>
             
-                    <div class="flex items-center justify-center md:justify-center md:pl-32">
-                        <img class="md:w-96 w-60" src="https://raw.githubusercontent.com/KavikaPalletenne/scantag-assets/main/scantag-mockup-edited2.png" alt=""/>
+                    <div className="flex items-center justify-center md:justify-center md:pl-32">
+                        <img className="md:w-96 w-60" src="https://raw.githubusercontent.com/KavikaPalletenne/scantag-assets/main/scantag-mockup-edited2.png" alt=""/>
                     </div>
                 </div>
             </div>
         </header>
         
         <section>
-            <div class="container max-w-7xl mx-auto pb-20 lg:pb-40 p-4 sm:p-6 pt-20 lg:pt-20 lg:p-8 bg-white dark:bg-gray-800">
-                <div class="flex flex-wrap -mx-4">
-                    <div class="w-full lg:w-1/2 px-8">
-                        <div class="mb-12 lg:pt-10 lg:mb-0 pb-12 lg:pb-0 border-b lg:border-b-0">
-                            <h2 class="mb-4 text-3xl lg:text-4xl font-bold font-heading dark:text-white">
+            <div className="container max-w-7xl mx-auto pb-20 lg:pb-40 p-4 sm:p-6 pt-20 lg:pt-20 lg:p-8 bg-white dark:bg-gray-800">
+                <div className="flex flex-wrap -mx-4">
+                    <div className="w-full lg:w-1/2 px-8">
+                        <div className="mb-12 lg:pt-10 lg:mb-0 pb-12 lg:pb-0 border-b lg:border-b-0">
+                            <h2 className="mb-4 text-3xl lg:text-4xl font-bold font-heading dark:text-white">
                                 Ease of use, peace of mind.
                             </h2>
-                            <p class="mb-8 leading-loose text-gray-500 dark:text-gray-300">
+                            <p className="mb-8 leading-loose text-gray-500 dark:text-gray-300">
                                 Simply register your account, then input your desired information. Your personalised QR code will then be generated. Use this on anything you wish; your wallet, luggage, even your dog. Whenever someone scans your code, we send you a notification so you know someone's found it.
                             </p>
                         </div>
                     </div>
-                    <div class="w-full lg:w-1/2 px-8">
-                        <ul class="space-y-12">
-                            <li class="flex -mx-4">
-                                <div class="px-4">
-                                    <span class="flex w-16 h-16 mx-auto items-center justify-center text-2xl font-bold font-heading rounded-full" style={{backgroundColor: "#ffebe3", color: "#FF9369"}}>
+                    <div className="w-full lg:w-1/2 px-8">
+                        <ul className="space-y-12">
+                            <li className="flex -mx-4">
+                                <div className="px-4">
+                                    <span className="flex w-16 h-16 mx-auto items-center justify-center text-2xl font-bold font-heading rounded-full" style={{backgroundColor: "#ffebe3", color: "#FF9369"}}>
                                         1
                                     </span>
                                 </div>
-                                <div class="px-4">
-                                    <h3 class="my-1 text-xl font-semibold dark:text-white">
+                                <div className="px-4">
+                                    <h3 className="my-1 text-xl font-semibold dark:text-white">
                                         Easily customise your information at anytime, on any device.
                                     </h3>
                                 </div>
                             </li>
-                            <li class="flex -mx-4">
-                                <div class="px-4">
-                                    <span class="flex w-16 h-16 mx-auto items-center justify-center text-2xl font-bold font-heading rounded-full" style={{backgroundColor: "#ffebe3", color: "#FF9369"}}>
+                            <li className="flex -mx-4">
+                                <div className="px-4">
+                                    <span className="flex w-16 h-16 mx-auto items-center justify-center text-2xl font-bold font-heading rounded-full" style={{backgroundColor: "#ffebe3", color: "#FF9369"}}>
                                         2
                                     </span>
                                 </div>
-                                <div class="px-4">
-                                    <h3 class="my-1 text-xl font-semibold dark:text-white">
+                                <div className="px-4">
+                                    <h3 className="my-1 text-xl font-semibold dark:text-white">
                                         With no upfront costs, you only pay when you need stickers.
                                     </h3>
                                 </div>
                             </li>
-                            <li class="flex -mx-4">
-                                <div class="px-4">
-                                    <span class="flex w-16 h-16 mx-auto items-center justify-center text-2xl font-bold font-heading rounded-full" style={{backgroundColor: "#ffebe3", color: "#FF9369"}}>
+                            <li className="flex -mx-4">
+                                <div className="px-4">
+                                    <span className="flex w-16 h-16 mx-auto items-center justify-center text-2xl font-bold font-heading rounded-full" style={{backgroundColor: "#ffebe3", color: "#FF9369"}}>
                                         3
                                     </span>
                                 </div>
-                                <div class="px-4">
-                                    <h3 class="my-1 text-xl font-semibold dark:text-white">
+                                <div className="px-4">
+                                    <h3 className="my-1 text-xl font-semibold dark:text-white">
                                         We keep your private and personal information locked and secure. 
                                     </h3>
                                 </div>
@@ -111,32 +111,32 @@ export default function Home() {
             </div>
         </section>
         
-        <footer class="dark:bg-gray-800 w-full py-8" style={{backgroundColor: "#FF9369"}}>
-            <div class="max-w-screen-xl mx-auto px-4">
-                <ul class="max-w-screen-md mx-auto text-lg font-light flex flex-wrap justify-between">
-                    <li class="my-2">
-                        <a class="text-white hover:text-gray-200 dark:text-gray-300 dark:hover:text-white transition-colors duration-200" href="/faq">
+        <footer className="dark:bg-gray-800 w-full py-8" style={{backgroundColor: "#FF9369"}}>
+            <div className="max-w-screen-xl mx-auto px-4">
+                <ul className="max-w-screen-md mx-auto text-lg font-light flex flex-wrap justify-between">
+                    <li className="my-2">
+                        <a className="text-white hover:text-gray-200 dark:text-gray-300 dark:hover:text-white transition-colors duration-200" href="/faq">
                             FAQ
                         </a>
                     </li>
-                    <li class="my-2">
-                        <a class="text-white hover:text-gray-200 dark:text-gray-300 dark:hover:text-white transition-colors duration-200" href="/site-map">
+                    <li className="my-2">
+                        <a className="text-white hover:text-gray-200 dark:text-gray-300 dark:hover:text-white transition-colors duration-200" href="/site-map">
                             Site Map
                         </a>
                     </li>
-                    <li class="my-2">
-                        <a class="text-white hover:text-gray-200 dark:text-gray-300 dark:hover:text-white transition-colors duration-200" href="/contact">
+                    <li className="my-2">
+                        <a className="text-white hover:text-gray-200 dark:text-gray-300 dark:hover:text-white transition-colors duration-200" href="/contact">
                             Contact
                         </a>
                     </li>
-                    <li class="my-2">
-                        <a class="text-white hover:text-gray-200 dark:text-gray-300 dark:hover:text-white transition-colors duration-200" href="/legal">
+                    <li className="my-2">
+                        <a className="text-white hover:text-gray-200 dark:text-gray-300 dark:hover:text-white transition-colors duration-200" href="/legal">
                             Legal
                         </a>
                     </li>
                 </ul>
                 
-                <div class="text-center text-white dark:text-gray-200 pt-10 sm:pt-12 font-light flex items-center justify-center">
+                <div className="text-center text-white dark:text-gray-200 pt-10 sm:pt-12 font-light flex items-center justify-center">
                     Created by <a href="https://kavika.dev" className="pl-1 hover:text-gray-200 transition-colors duration-200">Kavika</a>
                 </div>
             </div>
