adb58f6c135b03eaec634550655d523b489d5345
Kavika Palletenne <kbpalletenne@gmail.com>
2021-04-24 12:33:35 +1000

    test for rounded box

--- pages/register.tsx
+++ pages/register.tsx
@@ -38,7 +38,7 @@ export default function Home() {
 
 
         <div className="min-h-screen flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8" style={{backgroundImage: `url(https://raw.githubusercontent.com/KavikaPalletenne/scantag-assets/main/aurora-background4.png)`}}>
-            <div className="max-w-md w-full space-y-8 bg-gray-50 rounded-2xl p-10">
+            <div className="space-y-8 bg-gray-50 rounded-2xl p-10">
                 <div>
                 <Link href="/">
                     <a>
