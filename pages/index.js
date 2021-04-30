212fdf14d922e725fe10952d57b65c82eb98284f
Kavika Palletenne <kbpalletenne@gmail.com>
2021-04-30 21:30:06 +1000

    only runs useEffect() once

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,7 @@ export default function Home() {
         }
 
         CheckLoggedIn()
-    })
+    }, [])
   
   
     return (
--- pages/login.tsx
+++ pages/login.tsx
@@ -28,7 +28,7 @@ export default function Login() {
         }
 
         CheckLoggedIn()
-    })
+    }, [])
 
     
     
