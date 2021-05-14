12fadc7c19d7ec99ad8e6fd97e25c1c57bda3c36
Kavika Palletenne <kbpalletenne@gmail.com>
2021-05-14 10:14:10 +1000

    redirect to /account after login

--- pages/auth/flow/login.tsx
+++ pages/auth/flow/login.tsx
@@ -54,7 +54,7 @@ export default function Login() {
                         return
                     }
 
-                    router.push("/account/tags")
+                    router.push("/account")
                     }
                 });
 
