001b51e3b52a78afd9973ad0954a73d9b124ed6e
Kavika Palletenne <kbpalletenne@gmail.com>
2021-06-14 17:19:20 +1000

    updated mock up positioning

--- pages/404.js
+++ pages/404.js
@@ -17,26 +17,26 @@ export default function Custom404() {
         
         <header className="h-screen min-h-full overflow-hidden">
             
-            <div className="xl:pt-10 xl:pl-32 text-white xl:pr-32 pt-10 pl-5 pr-5">
+            <div className="lg:pt-10 lg:pl-32 text-white lg:pr-32 pt-10 pl-5 pr-5">
                 <Link href="/">
                 <a>
-                    <img className="float-left xl:w-20 w-12" src="https://raw.githubusercontent.com/KavikaPalletenne/scantag-assets/main/scantag-logo.svg"/>
+                    <img className="float-left lg:w-20 w-12" src="https://raw.githubusercontent.com/KavikaPalletenne/scantag-assets/main/scantag-logo.svg"/>
                 </a>
                 </Link>
             </div>
 
-            <div className="container xl:pl-32 xl:pt-48 pt-24 pl-5">
-                <div className="items-center xl:flex">
-                    <div className="w-full xl:w-2/3">
+            <div className="container lg:pl-32 lg:pt-48 pt-24 pl-5">
+                <div className="items-center lg:flex">
+                    <div className="w-full lg:w-2/3">
                         <div className="max-w-2xl pr-4">
-                        <h1 className="text-7xl font-bold text-orange dark:text-white xl:text-9xl" style={{fontFamily: 'Rubik'}}>404</h1>
-                            <h1 className="text-3xl font-bold text-white dark:text-white xl:text-7xl" style={{fontFamily: 'Rubik'}}>Page not found</h1>
-                            <p className="mt-2 xl:text-4xl text-xl text-gray-300 dark:text-gray-400 pb-5">If only it had a ScanTag...</p>
+                        <h1 className="text-7xl font-bold text-orange dark:text-white lg:text-9xl" style={{fontFamily: 'Rubik'}}>404</h1>
+                            <h1 className="text-3xl font-bold text-white dark:text-white lg:text-7xl" style={{fontFamily: 'Rubik'}}>Page not found</h1>
+                            <p className="mt-2 lg:text-4xl text-xl text-gray-300 dark:text-gray-400 pb-5">If only it had a ScanTag...</p>
                         </div>
                     </div>
             
-                    <div className="fixed bottom-0 right-32">
-                        <img className="xl:w-96 w-60 max-h-screen" src="https://raw.githubusercontent.com/KavikaPalletenne/scantag-assets/main/scantag-mockup-edited2.png" alt=""/>
+                    <div className="lg:pt-0 lg:pl-0 pl-10 pt-5 lg:absolute lg:bottom-0 lg:right-20">
+                        <img className="lg:w-96 w-60 max-h-screen" src="https://raw.githubusercontent.com/KavikaPalletenne/scantag-assets/main/scantag-mockup-edited2.png" alt=""/>
                     </div>
                 </div>
             </div>
